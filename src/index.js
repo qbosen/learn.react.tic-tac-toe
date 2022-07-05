6d726cb84808cc622b43a1244a6c430c417125d5
qiubaisen <abosen@qq.com>
2022-07-05 16:33:50 +0800

    使用循环来渲染棋盘

--- src/index.js
+++ src/index.js
@@ -14,29 +14,24 @@ class Board extends React.Component {
 
   renderSquare(i) {
     return <Square
+      key={"square" + i}
       value={this.props.squares[i]}
       onClick={() => this.props.onClick(i)}
     />;
   }
 
   render() {
+    const n = 3;
+    const array = [...Array(n).keys()];
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {
+          array.map((i) => (
+            <div key={"board-row" + i} className="board-row">
+              {array.map((j) => this.renderSquare(i * n + j))}
+            </div>
+          ))
+        }
       </div>
     );
   }
